42f8b4ae486d3fef49151d853f079c364a805704
Andrey Romanov <andrey.a.romanov@gmail.com>
2015-12-29 02:02:01 +0300

    debug

--- index.php
+++ index.php
@@ -77,6 +77,7 @@ if (isset($_GET['msg']) && !empty($_GET['msg'])) {
 
 session_id($chat);
 session_name($chat);
+session_cache_expire(1);
 session_start();
 echo ini_get('session.name');
 echo session_id();
